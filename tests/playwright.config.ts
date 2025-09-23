10b75a34245c5ae1f049eefc98e85a832b022950
khaimov <roman.khaimov@reksoft.com>
2025-09-23 19:14:45 +0300

    add masked

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   fullyParallel: false,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
